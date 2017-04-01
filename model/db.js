c80bf27da9b902872ebe8232c06e15d95162567a
livefree1 <edy.alva_101@hotmail.com>
2017-04-01 18:20:52 -0400

    Fixed date thing

--- model/db.js
+++ model/db.js
@@ -1,3 +1,4 @@
 var mongoose = require('mongoose');
 //mongoose.Promise = global.Promise;
+//mongoose.connect('mongodb://130.245.168.122/twitter_db');
 mongoose.connect('mongodb://192.168.1.20/twitter_db');
