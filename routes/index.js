536f23055fb41cb3fc3d60b49a61cdabab2b10be
Sajid Kamal <sajidkamal@Sajids-MacBook-Air.local>
2017-04-03 22:00:58 -0400

    testing

--- routes/index.js
+++ routes/index.js
@@ -24,8 +24,13 @@ router.get('/search',function(req, res, next){
   res.render('search');
 });
 
+router.get('/search',function(req, res, next){
+  res.render('search');
+});
 
-
+router.get('/lb',function(req,res,next){
+  res.send('LOAD BALANCING!!!!');
+})
 // --------------ITEM LOGIC --> tweetHelper.js
 router.post('/additem', ensureAuthenticated, addItem);
 router.post('/search', ensureAuthenticated, searchItem);
