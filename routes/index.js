e47c48ebd49254b1fbaa2d4265559c2a1726b422
Sajid Kamal <sajidkamal@Sajids-MacBook-Air.local>
2017-04-06 18:28:48 -0400

    ok

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.get('/search',function(req, res, next){
 });
 
 router.get('/lb',function(req,res,next){
-  res.send('Good Vibes!!');
+  res.send('littyyyyyyy');
 })
 // --------------ITEM LOGIC --> tweetHelper.js
 router.post('/additem', ensureAuthenticated, addItem);
