b68aa5ae95ea1ddffb565030101fbb42b0865ec9
Sajid Kamal <sajidkamal@Sajids-MacBook-Air.local>
2017-04-20 15:52:10 -0400

    OK

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(logger('dev')); // COMMENT THIS OUT FOR THE SCRIPT!!!!!!!
+app.use(logger('dev')); // COMMENT THIS OUT FOR THE SCRIPT!!!!!!!
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
