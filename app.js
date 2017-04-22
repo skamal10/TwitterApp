46fc553f7d9a0b37da12f252b265151b640d1040
livefree1 <edy.alva_101@hotmail.com>
2017-04-22 19:51:56 -0400

    render error messasage

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render(err.message);
 });
 
 module.exports = app;
