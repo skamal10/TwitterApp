9b02c81804959a707479fb91ec43f3cc2687e38b
Sajid Kamal <sajidkamal@Sajids-Air.home>
2017-04-22 10:31:44 -0400

    OK

--- model/Item.js
+++ model/Item.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+autoIncrement.initialize(mongoose);
+
 var itemSchema = new mongoose.Schema({
     
     username: {
@@ -15,7 +18,7 @@ var itemSchema = new mongoose.Schema({
 	 default: Date.now 
     },
     parent:{
-    	type:  mongoose.Schema.Types.ObjectId
+    	type:  Number
     },
     media: [mongoose.Schema.Types.ObjectId],
     likes: [mongoose.Schema.Types.ObjectId]
@@ -27,4 +30,5 @@ itemSchema.set('toJSON', {
 });
 itemSchema.index( {content: "text"} );
 itemSchema.index( {times: -1});
+itemSchema.plugin(autoIncrement.plugin, 'Item');
 mongoose.model('Item', itemSchema);
