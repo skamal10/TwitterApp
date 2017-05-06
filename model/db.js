379b17b0013929a55431f3d157d440a9f2f902d5
Sajid Kamal <sajidkamal@Sajids-Air.home>
2017-05-06 19:46:34 -0400

    OK

--- model/db.js
+++ model/db.js
@@ -2,5 +2,5 @@ var mongoose = require('mongoose');
 //mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://130.245.168.122/twitter_db');
 //mongoose.connect('mongodb://192.168.1.32/twitter_db');
-mongoose.connect('mongodb://192.168.1.47:27018/twitter_db', { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } } });
+mongoose.connect('mongodb://192.168.1.55:27018/twitter_db', { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } } });
 
